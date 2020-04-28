3fc7d3b24e9a01f5ce9642f25c5486faa73fb139
Celso junior <celsoestercio.jr@gmail.com>
2020-04-27 21:18:05 -0300

    Create resource exception

--- src/main/java/com/java/assistencia/controller/handlers/ResourceExceptionHandler.java
+++ src/main/java/com/java/assistencia/controller/handlers/ResourceExceptionHandler.java
@@ -3,6 +3,7 @@ package com.java.assistencia.controller.handlers;
 import com.java.assistencia.exception.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -17,6 +18,13 @@ public class ResourceExceptionHandler extends ResponseEntityExceptionHandler {
         ApiError error = new ApiError(HttpStatus.NOT_FOUND.value(), ex.getMessage(), new Date());
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<ApiError> handlerBadCredentialsException(BadCredentialsException ex) {
+        ApiError error = new ApiError(HttpStatus.UNAUTHORIZED.value(), ex.getMessage(), new Date());
+
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(error);
     }
 
 }
